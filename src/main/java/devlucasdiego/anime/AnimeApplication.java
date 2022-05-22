9865e7ca4fd61ebf67543fcc3f34a949626527be
Lucas Diego <luucasdiego_12@hotmail.com>
2022-05-22 18:46:09 -0400

    Classe Anime

--- src/main/java/devlucasdiego/anime/AnimeApplication.java
+++ src/main/java/devlucasdiego/anime/AnimeApplication.java
@@ -1,13 +1,15 @@
 package devlucasdiego.anime;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableAutoConfiguration
 @SpringBootApplication
 public class AnimeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AnimeApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AnimeApplication.class, args);
+    }
 
 }
---
+++ src/main/java/devlucasdiego/anime/domain/Anime.java
@@ -0,0 +1,14 @@
+package devlucasdiego.anime.domain;
+
+public class Anime {
+    private String name;
+
+    public Anime() {
+
+    }
+
+    public Anime(String name) {
+        this.name = name;
+    }
+
+}
