0ac2ae1bd60e4ac3a8471aee127d28ca1963dc4c
Lucas Diego <luucasdiego_12@hotmail.com>
2022-05-24 22:23:24 -0400

    Annotations

--- src/main/java/devlucasdiego/anime/controller/AnimeController.java
+++ src/main/java/devlucasdiego/anime/controller/AnimeController.java
@@ -1,17 +1,26 @@
 package devlucasdiego.anime.controller;
 
 import devlucasdiego.anime.domain.Anime;
+import devlucasdiego.anime.util.DateUtil;
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
 @RequestMapping("anime")
+@Log4j2
+@AllArgsConstructor
 public class AnimeController {
+    private DateUtil dateUtil;
+
     @GetMapping(path = "list")
     public List<Anime> list() {
+        log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
         return List.of(new Anime("Steins Gate"), new Anime("Demon Slayer"), new Anime("Cowboy Bebop"));
     }
 }
